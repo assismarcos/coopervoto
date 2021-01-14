86b235ff08c5c06656ade3acfa2402d4f3ed2d85
Rogério Disciolli <disciolli@gmail.com>
2021-01-14 11:43:57 -0300

    Estrutura inicial

---
+++ src/test/java/com/disciolli/coopervoto/CoopervotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disciolli.coopervoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopervotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
